0abda9a5ef64d6200150dbf65bdc65b97861c14f
RyabovSO <ryabov.ser.o@gmail.com>
2018-02-13 23:59:32 +0300

    создал сервер Hello World

--- server.go
+++ server.go
@@ -1,1 +1,16 @@
-//мой сервер
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Hello World")
+}
+
+func main() {
+	fmt.Println("Listening on port :3000")
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":3000", nil)
+}
